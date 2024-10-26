3d2143dcef34f9ed9cb4c4a1d6018ca796065ac7
Gaia <gaia.clerici.01@gmail.com>
2024-10-26 18:43:14 +0200

    Map show

---
+++ test.py
@@ -0,0 +1,48 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import geopandas as gpd
+import dash_bootstrap_components as dbc
+
+#Create and populate the map
+df = px.data.gapminder()
+df = df[df['continent'] == 'Europe']
+#If we want latitude and longitude of points remove locations and write "lat=geo_df.geometry.y, lon=geo_df.geometry.x"
+fig = px.scatter_geo(df, locations="iso_alpha", color="continent", hover_name="country",
+               animation_frame="year", projection="natural earth")
+
+fig.update_geos(scope = 'europe', resolution=50)
+fig.update_layout(height=500, margin={"r":0,"t":0,"l":0,"b":0})
+
+#Create the Dash app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div(
+    [
+        dbc.Row(dbc.Col(
+            html.Div("Water quality in Europe", 
+            style={
+                "text-align": "center",
+                "margin": "6px",
+                "color": "rgb(0, 0, 153)",
+                "font-size":"25px",
+                "padding-bottom": "6px",
+                "border-bottom-width": "2px",
+                "border-bottom-color": "rgb(0, 0, 153)",
+                "border-bottom-style": "solid"}
+            ),
+            width={"size": 12},
+        )),
+        dbc.Row([
+            dbc.Col(
+                html.Div(dcc.Graph(figure=fig)),
+                width={"size": 9}
+            ),
+            dbc.Col(
+                html.Div("here graphs"),
+                width={"size": 2}
+            )
+        ])
+    ]
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
