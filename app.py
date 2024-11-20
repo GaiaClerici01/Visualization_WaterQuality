a96da5dd47ea29e3f894c32bb97426cda658a80d
Gaia <gaia.clerici.01@gmail.com>
2024-11-20 16:46:57 +0100

    animation

--- app.py
+++ app.py
@@ -3,29 +3,18 @@ import plotly.graph_objs as go
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-# Ask Gareth: Animation with go? What do we do about outliers?
-
-
-#Keep this code for future animation
-'''df_geo = px.data.gapminder()
-df_geo = df_geo[df_geo['continent'] == 'Europe']
-#If we want latitude and longitude of points remove locations and write "lat=geo_df.geometry.y, lon=geo_df.geometry.x"
-fig = px.scatter_geo(df_geo, locations="iso_alpha", color="continent", hover_name="country",
-               animation_frame="year", projection="natural earth")
-'''
+# Load data
 df_sites = pd.read_csv('monitoringSite.csv')
 df_data22 = pd.read_csv('aggregateddata2022.csv')
 df_data22['year'] = 2022
 df_data21 = pd.read_csv('aggregateddata2021.csv')
 df_data21['year'] = 2021
-df_data = [df_data21, df_data22]
-df_data = pd.concat(df_data)
+df_data = pd.concat([df_data21, df_data22])
 
 df_geo = pd.merge(df_sites, df_data, on='monitoringSiteIdentifier')
 
-# Extract unique indicators and calculate weighted mean for density map
+# Extract unique indicators
 df_elements = df_geo[['eeaIndicator']].drop_duplicates()
-df_weighted_mean = df_geo.groupby('eeaIndicator')['resultMeanValue'].mean()
 
 # Create the Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -50,86 +39,168 @@ app.layout = html.Div(
                 width={"size": 12},
             )
         ),
+        
         dbc.Row(
-            dbc.Col(
-                dcc.Dropdown(
-                    id='element_filter',
-                    options=[{'label': i, 'value': i} for i in df_elements['eeaIndicator']],
-                    value='Nitrate',
-                    clearable=False,
-                    style={
-                        'margin-bottom': '2'
-                    }
-                )
-            )
-        ),
-        dbc.Row(
-            [
+           [
                 dbc.Col(
                     dcc.Graph(id='map_graph'),
                     width={"size": 8},
                 ),
-                dbc.Col(
-                    dcc.Graph(id='line_graph'),
-                    width={"size": 4},
-                ),
+                dbc.Col([
+                    dbc.Row(
+                       dcc.Dropdown(
+                            id='element_filter',
+                            options=[
+                                {'label': i, 'value': i} for i in df_elements['eeaIndicator']
+                            ],
+                            value='Nitrate',
+                            clearable=False,
+                            style={
+                                "margin-bottom": "10px",
+                                "margin-top": "10px",
+                                "width": "90%",  # Adjust width
+                                "margin-left": "auto",
+                                "margin-right": "auto"
+                            }
+                        )
+                    ),
+                    dbc.Row(
+                        dcc.Graph(id='line_graph'),
+                    ),
+                ],),
             ]
-        ),
+        )
     ]
 )
 
-# Callback to update the density mapbox with weighted mean coloring
+# Callback to update the animated map
 @app.callback(
     Output('map_graph', 'figure'),
     Input('element_filter', 'value')
 )
 def update_map(selected_element):
     filtered_data = df_geo[df_geo['eeaIndicator'] == selected_element]
-    hovertemplate = '%{text}<br>Value: %{z} </span><extra></extra>'
 
-    fig = go.Figure()
+    # Create the map figure
+    years = sorted(filtered_data['year'].unique())
 
-    fig.add_trace(go.Scattermapbox(
-        lat=filtered_data['lat'],
-        lon=filtered_data['lon'],
-        marker=dict(
-            size=5,
-            color=filtered_data['resultMeanValue'],
-            colorscale='matter',
-            cmin=filtered_data['resultMeanValue'].min(),
-            cmax=filtered_data['resultMeanValue'].max(),
+    # Traces for the initial year
+    initial_data = filtered_data[filtered_data['year'] == years[0]]
+    hovertemplate = '<b>%{text}</b><br>Value: %{z:.2f}<extra></extra>'
+    #TODO understand which trace is better
+    traces = [
+        go.Scattermapbox(
+            lat=initial_data['lat'],
+            lon=initial_data['lon'],
+            mode='markers',
+            marker=dict(
+                size=8,
+                color=initial_data['resultMeanValue'],
+                colorscale='matter',
+                cmin=filtered_data['resultMeanValue'].min(),
+                cmax=filtered_data['resultMeanValue'].max(),
+            ),
+            text=initial_data['waterBodyName'],
+            name=str(years[0]),
+            hovertemplate=hovertemplate
         ),
-        opacity=0.7,  # Adjust opacity as needed
-    ))
+        go.Densitymapbox(
+            lat=filtered_data['lat'],
+            lon=filtered_data['lon'],
+            text=filtered_data['waterBodyName'],
+            z=filtered_data['resultMeanValue'],
+            radius=8,
+            opacity=1,  # Adjust opacity as needed
+            colorscale="matter",
+            colorbar=dict(title="Sample value"),
+            zmin=filtered_data['resultMeanValue'].min(),
+            zmax=filtered_data['resultMeanValue'].max(),
+            hovertemplate=hovertemplate
+        )
+    ]
 
-    # Create the density mapbox using weighted mean for coloring
-    fig.add_trace(go.Densitymapbox(
-        lat=filtered_data['lat'],
-        lon=filtered_data['lon'],
-        text=filtered_data['waterBodyName'],
-        z=filtered_data['resultMeanValue'],
-        radius=8,
-        opacity=1,  # Adjust opacity as needed
-        colorscale="matter",
-        colorbar=dict(title="Sample value"),
-        zmin=filtered_data['resultMeanValue'].min(),
-        zmax=filtered_data['resultMeanValue'].max(),
-        hovertemplate=hovertemplate
-    ))
-    
+    # Frames for each year
+    frames = [
+        go.Frame(
+            data=[
+                go.Scattermapbox(
+                    lat=filtered_data[filtered_data['year'] == year]['lat'],
+                    lon=filtered_data[filtered_data['year'] == year]['lon'],
+                    mode='markers',
+                    marker=dict(
+                        size=8,
+                        color=filtered_data[filtered_data['year'] == year]['resultMeanValue'],
+                        colorscale='matter',
+                        cmin=filtered_data['resultMeanValue'].min(),
+                        cmax=filtered_data['resultMeanValue'].max(),
+                    ),
+                    text=filtered_data[filtered_data['year'] == year]['waterBodyName']
+                )
+            ],
+            name=str(year)
+        )
+        for year in years
+    ]
 
-    # Configure the map
-    fig.update_layout(
-        mapbox=dict(
-            style="carto-darkmatter",
-            center={"lat": 42.5, "lon": 12.5},  # Focus on Italy
-            zoom=5,
-        ),
-        margin={"r": 3, "t": 6, "l": 3, "b": 0},
-        height=500,
+    # Configure map layout
+    layout = go.Layout(
+    mapbox=dict(
+        style="carto-darkmatter",
+        center={"lat": 42.5, "lon": 12.5},  # Focus on Italy
+        zoom=5,
+    ),
+    margin={"r": 3, "t": 6, "l": 3, "b": 0},
+    height=700,
+    sliders=[
+        {
+            "active": 0,
+            "pad": {"t": 50, "l": 20, "b":10},  # Adds padding above the slider for the buttons
+            "x": 0.1,  # Adjust the position of the slider to fit the buttons
+            "y": -0.05,
+            "len": 0.8,  # Adjust the length of the slider line
+            "steps": [
+                {
+                    "method": "animate",
+                    "args": [[str(year)], {"frame": {"duration": 500, "redraw": True}, "mode": "immediate"}],
+                    "label": str(year)
+                }
+                for year in years
+            ],
+            "currentvalue": {
+                "visible": True,
+                "prefix": "Year: ",  # Label for the current value
+                "xanchor": "center",
+                "font": {"size": 14},
+            },
+        }
+    ],
+    updatemenus=[
+        {
+            "type": "buttons",
+            "x": 0.57,  # Position the buttons next to the slider
+            "y": 0.05,  # Align the buttons at the beginning of the slider
+            "direction": "left",
+            "buttons": [
+                {
+                    "args": [None, {"frame": {"duration": 500, "redraw": True}, "fromcurrent": True}],
+                    "label": "▶",  
+                    "method": "animate"
+                },
+                {
+                    "args": [[None], {"frame": {"duration": 0, "redraw": True}, "mode": "immediate"}],
+                    "label": "⏸",
+                    "method": "animate"
+                }
+            ],
+            "showactive": True,
+            "pad": {"r": 10, "t": 50},  # Adjust padding for aesthetics
+        }
+    ]   
     )
-     
 
+
+    # Build and return the figure
+    fig = go.Figure(data=traces, layout=layout, frames=frames)
     return fig
 
 # Callback per aggiornare il grafico temporale
@@ -150,10 +221,9 @@ def update_line_graph(selected_element):
     ))
 
     line_fig.update_layout(
-        title="Trend nel tempo",
-        xaxis_title="Anno",
-        yaxis_title="Valore medio",
-        height=500
+        title="Annual trend",
+        xaxis_title="Year",
+        yaxis_title="Value",
     )
 
     return line_fig
